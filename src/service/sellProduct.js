faaf34fd586fbd9a301db001f828a7ab10e12081
Veríssimo Casas <Verissimo0073@gmail.com>
2022-10-06 14:38:37 -0400

    Base do Projeto para o CircleCI

---
+++ src/service/sellProduct.js
@@ -0,0 +1,12 @@
+/**
+ * A quantidade vendida pode ser de 1 ou mais unidades
+ * --> Se o estoque ficar negativo uma exception deve ser lancada
+ * --> O valor de venda nao pode ser maior que valor de compra.
+ * @param {*} product 
+ * @param {*} amount 
+ */
+
+export default function sellProduct(product, amount) {
+  product.stock -= 1;
+  return product;
+}
